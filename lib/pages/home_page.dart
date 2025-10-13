8e1e6ef6f4cc93e9a0b357390a2b89c590b180fb
Maria <mariabrodowska89@gmail.com>
2025-10-13 17:53:58 +0200

    Add initial implementation of HomePage and setup routing; create empty CardsPage and SetupPage files

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(Icons.visibility_off, size: 100, color: Colors.indigo),
+            const SizedBox(height: 32),
+            const Text(
+              "Undercover Game",
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/setup');
+              },
+              child: const Text("Start Game"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
